fde25e59c983d11c673c2ca8e186f9809babb377
Matt Boothby <matt@boothby.me>
2024-02-10 01:15:37 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        whiskey: {
+          50: "#fcf7f0",
+          100: "#f8ecdc",
+          200: "#efd5b9",
+          300: "#e5b78c",
+          400: "#dd9e6e",
+          500: "#d1773e",
+          600: "#c36133",
+          700: "#a24c2c",
+          800: "#823e2a",
+          900: "#693525",
+          950: "#381912",
+        },
+      },
+      fontFamily: {
+        press2p: ['"Press Start 2P"', "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+};
