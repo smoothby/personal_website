d4d0d6843ed5ff078ec6925e7c00e824216fe9ac
Matt Boothby <matt@boothby.me>
2024-02-14 00:19:19 -0500

    Added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        pulse: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
+        },
+      },
+      animation: {
+        fullpulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
       colors: {
         whiskey: {
           50: "#fcf7f0",
@@ -19,7 +28,8 @@ module.exports = {
         },
       },
       fontFamily: {
-        press2p: ['"Press Start 2P"', "system-ui"],
+        alfa: ['"Alfa Slab One"', "serif"],
+        opensans: ['"Open Sans"', "sans-serif"],
       },
     },
   },
